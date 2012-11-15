1c0719175930b1cb65b815068215d1a795c1e8d1
Thomas Monzel <tom.monzel@gmail.com>
2012-11-15 14:16:56 +0100

    changed autoloading in initializer

--- lib/http.php
+++ lib/http.php
@@ -11,12 +11,8 @@
  * @subpackage http
  */
 namespace {
-    
-  # register default autoload functionality
-  /*spl_autoload_register(function($class) {
-    return spl_autoload(preg_replace('/(\p{Ll})(\p{Lu})/', '$1_$2', $class), '.php');
-  });*/
   
+  # register default autoload functionality  
   spl_autoload_register(function($class) {
     $underscored_class = preg_replace('/(\p{Ll})(\p{Lu})/', '$1_$2', $class);
     $file = __DIR__."/".str_replace('\\', '/', strtolower($underscored_class)).".php";
