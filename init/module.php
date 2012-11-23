6271235b4267e289db979d927b53601ceb2e1562
Thomas Monzel <tom.monzel@gmail.com>
2012-11-23 01:11:58 +0100

    Created init directory

--- init/module.php
+++ init/module.php
@@ -1,19 +1,19 @@
 <?php
 /**
  * Initializes the network suit
- * This core package can be used as an isolate unit from Suitcase. Just apply
+ * This core package can be used as an isolate unit from Battlesuit. Just apply
  * your own autoloading functionality if you will.
  *
  * PHP Version 5.3+
  * @author Thomas Monzel <tm@apparat-hamburg.de>
  * @version $Revision$
- * @package Suitcase
+ * @package Battlesuit
  * @subpackage http
  */
-namespace suitcase {
-  autoload_in('http', __DIR__."/lib/http");
+namespace bs {
+  autoload_in('http', dirname(__DIR__)."/lib/http");
   
   # import helper functions
-  require_once __DIR__.'/lib/http/functions.php';
+  require_once dirname(__DIR__).'/lib/http/functions.php';
 }
 ?>
---
+++ init/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Initializes the http testing environment
+ *
+ * PHP Version 5.3+
+ * @author Thomas Monzel <tm@apparat-hamburg.de>
+ * @version $Revision$
+ * @package Battlesuit
+ * @subpackage http
+ */
+namespace bs {
+  require '../../module.php';
+  import('test', 'http');
+}
+?>
